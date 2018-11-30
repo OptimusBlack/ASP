5c5b2130c4aa2bd267d6f536cce1dd28edf81822
utkarsh867 <utkarsh867@gmail.com>
2018-11-30 23:54:07 +0800

    fix CSV output QMH duplicate lines

--- dispatcher/views.py
+++ dispatcher/views.py
@@ -113,6 +113,5 @@ def dispatch(request):
         writer.writerow([location_data.lat, location_data.lng, location_data.alt])
     location_data = LocationData.objects.get(name='Queen Mary Hospital Drone Port')
     writer.writerow([location_data.lat, location_data.lng, location_data.alt])
-    writer.writerow([location_data.lat, location_data.lng, location_data.alt])
     request.session['dispatch'] = []
     return response
